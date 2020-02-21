22acf6af35bb460cab5f020476d51bb22ac8ce72
ArthurYoon <arthur.yoon@wirebarley.com>
2020-02-21 10:58:37 +0900

    init

---
+++ src/main/java/com/dev/toycore/ToyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dev.toycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/toycore/ToyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.toycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
